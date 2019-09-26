12de6779efb329fa602efc7555a08a586f466698
Healer2202 <cubi121212@gmail.com>
2019-09-26 14:30:23 +0700

    ex04

---
+++ ex04/ex04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i <= 100; i++)
+            {
+                if()
+                {
+                    Console.WriteLine(i);
+                }
+                //else
+                //{
+                //    Console.WriteLine()
+               // }
+            }
+        }
+        static bool IsOdd(int n)
+        {
+            return n % 2 != 0;
+        }
+        static bool IsEven(int n)
+        {
+            return n % 2 == 0;
+        }
+    }
+}
